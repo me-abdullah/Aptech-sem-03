d40992470b1e268d7f31d26cd1918fc2228bbfe5
me-abdullah <enzoemrick@gmail.com>
2024-09-13 22:53:50 +0500

    Learning

---
+++ C# Learning/Class 01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+            Console.WriteLine("Hello we are learning C#");
+            Console.ReadKey();
+        }
+    }
+}
