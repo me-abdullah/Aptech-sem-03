c87b1dab73e93565af4c7fedda7da167bba51aaa
me-abdullah <enzoemrick@gmail.com>
2024-09-18 17:04:02 +0500

    Learning

---
+++ C# Learning/Class 02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#");
+            Console.WriteLine("Hello we are learning C#");
+            
+            // Variables & Data Types
+            int age = 25;
+            Console.WriteLine(age);
+
+            char letter = 'A';
+            Console.WriteLine(letter);
+
+            string myText = "Hello";
+            Console.WriteLine(myText);
+
+            bool mybool = false;
+            Console.WriteLine(mybool);
+
+            double myDoubleNum = 5.99;
+            Console.WriteLine(myDoubleNum);
+
+            // User Input
+
+            Console.WriteLine("Enter your Name:");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine($"Name is: {userName}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C# Learning/Class 03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array
+
+            string[] user_arr = ["Abdullah", "Hassan", "Irfan", "Afzal"];
+            Console.WriteLine(user_arr);
+            foreach (var user_arry in user_arr)
+            {
+                Console.WriteLine(user_arry);
+            }
+
+            int[] numarray = [10, 9, 8, 7, 6];
+
+            foreach (var num_arry in numarray)
+            {
+                Console.WriteLine(num_arry);
+            }
+        }
+    }
+}
