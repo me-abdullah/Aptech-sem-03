8b1b5575902f22b1110ba6e1924955995b244692
me-abdullah <enzoemrick@gmail.com>
2024-09-18 19:48:59 +0500

    Learning

---
+++ C# Learning/Class 04/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conditional Statement
+            Console.WriteLine("Enter your Name");
+
+            string username = Console.ReadLine();
+
+            /* Console.WriteLine("Enter your Password");
+
+             string password = Console.ReadLine();
+
+
+              if (username == "Abdullah" && password == "12345" || username == "Irfan" && password == "12345")
+              {
+                  Console.WriteLine("Matched");
+              }
+              else
+              {
+                  Console.WriteLine("not Matched");
+              }
+              Console.WriteLine($"my name is {username} and my age is {age}");
+            
+
+            //Ternary Operator
+
+            string result = username == "Abdullah" ? "Login" : "Login Denied";
+            Console.WriteLine(result);
+
+            //Switch Case
+
+            switch (username)
+            {
+                case "Abdullah":
+                    Console.WriteLine("Matched");
+                    break;
+                case "Haseeb":
+                    Console.WriteLine("Matched");
+                    break;
+                case "AnasBhai":
+                    Console.WriteLine("Matched");
+                    break;
+                case "Asad":
+                    Console.WriteLine("Matched");
+                    break;
+                default:
+                    Console.WriteLine("Not Matched");
+                    break;
+            }*/
+            Console.ReadKey();
+        }
+    }
+}
