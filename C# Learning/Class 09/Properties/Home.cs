e0fedfd8461b467de1213f98ce46539609161cc6
me-abdullah <enzoemrick@gmail.com>
2024-10-21 20:33:02 +0500

    Updated

---
+++ C# Learning/Class 09/Properties/Home.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Class_05
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Your code here
+        }
+    }
+}
