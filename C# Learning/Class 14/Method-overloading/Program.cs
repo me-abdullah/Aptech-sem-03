fbd6d4b51d354546bf2b88c076bb10b645d0ffe5
me-abdullah <enzoemrick@gmail.com>
2024-11-07 14:32:38 +0500

    Added Class-14

---
+++ C# Learning/Class 14/Method-overloading/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_overloading
+{
+    class Program
+    {
+        // Method with no parameters
+        public void PrintMessage()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        // Method with one parameter
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine("Message: " + message);
+        }
+
+        // Method with two parameters
+        public void PrintMessage(string message, int times)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine("Message: " + message);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            // Calling different overloaded versions of PrintMessage
+            program.PrintMessage();                   // Output: Hello, World!
+            program.PrintMessage("Hello, C#!");       // Output: Message: Hello, C#!
+            program.PrintMessage("Repeat this", 3);   // Output: Message: Repeat this (printed 3 times)
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Learning/Class 14/Polymor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymor
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic animal sound");
+        }
+    }
+
+    // Derived class
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Another derived class
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Polymorphism: a single method behaves differently based on the object's type.
+            Animal myAnimal = new Animal();
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            myAnimal.MakeSound(); // Output: Some generic animal sound
+            myDog.MakeSound();    // Output: Bark
+            myCat.MakeSound();    // Output: Meow
+
+            Console.ReadLine();
+        }
+    }
+}
