8fb87776dc558f20fb53fa40a9c27140d787f250
me-abdullah <enzoemrick@gmail.com>
2024-12-13 23:36:17 +0500

    ViewBag

---
+++ ASP Learning/Class 04/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Class_04.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Class_04.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            ViewBag.Message = "Message From View Bag";
+            ViewBag.CurrentDate = DateTime.Now.ToLongDateString();
+            string[] fruits = { "Apple", "Mango", "Banana", "Orange" };
+            ViewBag.FruitsArray = fruits;
+            ViewBag.SportsList = new List<string>()
+            {
+                "Cricket",
+                "Football",
+                "Hockey",
+            };
+
+            Employee employe = new Employee();
+
+            employe.EmpId = 1;
+            employe.EmpName = "Abdullah";
+            employe.EmpDesignation = "Developer";
+            ViewBag.employee = employe;
+
+            return View();
+        }
+    }
+}
---
+++ ASP Learning/Class 04/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Class_04.Models
+{
+    public class Employee
+    {
+
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+
+        public string EmpDesignation { get; set; }
+    }
+}
