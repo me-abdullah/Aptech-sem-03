76107b3df10ec0de8151101176b7402cae3cbe63
me-abdullah <enzoemrick@gmail.com>
2024-12-21 14:34:37 +0500

    Validation

---
+++ ASP Learning/Class 07/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Web.Mvc;
+
+namespace Class_06.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string age, string email)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                ModelState.AddModelError("username", "User name is required");
+            }
+            if (string.IsNullOrWhiteSpace(age))
+            {
+                ModelState.AddModelError("age", "User age is required");
+            }
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                ModelState.AddModelError("email", "User email is required");
+            }
+            if (ModelState.IsValid == true)
+            {
+                ViewData["SuccessMessage"] = "<script>alert('Thank you for submitting form');</script>";
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
