9500a14fcbec70cd0c083ab0cbceffbab7419509
me-abdullah <enzoemrick@gmail.com>
2024-12-29 19:57:11 +0500

    API & DB

---
+++ ASP Learning/Class 09/App_Start/RouteConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Class_09
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Custom route for API
+            routes.MapRoute(
+                name: "API",
+                url: "api/{controller}/{action}/{id}",
+                defaults: new { id = UrlParameter.Optional }
+            );
+
+            // Default route
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ASP Learning/Class 09/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using Class_09.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+public class ProductsController : Controller
+{
+    // GET: api/products
+    public JsonResult GetProducts()
+    {
+        var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 750.00M },
+            new Product { Id = 2, Name = "Tablet", Price = 300.00M },
+            new Product { Id = 3, Name = "Smartphone", Price = 500.00M }
+        };
+
+        return Json(products, JsonRequestBehavior.AllowGet);
+    }
+}
